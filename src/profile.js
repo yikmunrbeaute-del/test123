5ddf18ab449e0b50af48bce6e7d5e8468e16c7c9
Testing 123 <yikmunrbeaute@gmail.com>
2026-06-04 11:12:19 +0800

    Update profile.js

--- src/profile.js
+++ src/profile.js
@@ -9,7 +9,7 @@ const animation = {
 };
 // Change your display name on tha landing display
 const header = {
-  name: "a Human",
+  name: "Shit",
 };
 const background = {
   // Options: Snow or Particle
